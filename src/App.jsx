5cd28986258389981e9aa20d3ffb7c2801ec87c1
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-06-08 18:09:57 +0300

    Added search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
-import contacts from "./contacts.json";
+import data from "./data.json";
 
 function App() {
-  const [contact, setContact] = useState(contacts);
+  const [contacts, setContacts] = useState(data);
+  const [filter, setFilter] = useState("");
+  const [filteredContacts, setFilteredContacts] = useState(data);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value.toLowerCase().trim();
+    setFilter(inputValue);
+  };
+
+  useEffect(() => {
+    const filtredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+    setFilteredContacts(filtredContacts);
+  }, [filter, contacts]);
 
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contact} />
+      <SearchBox handleChange={handleChange} />
+      <ContactList contacts={filteredContacts} />
     </div>
   );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,17 @@
-// import css from "./SearchBox.module.css";
+import css from "./SearchBox.module.css";
 
-const SearchBox = () => {};
+const SearchBox = ({ handleChange }) => {
+  return (
+    <>
+      <p className={css.searchBoxText}>Find contact by name</p>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="Enter name..."
+        className={css.searchBoxInput}
+      />
+    </>
+  );
+};
 
 export default SearchBox;
